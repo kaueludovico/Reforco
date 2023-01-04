1124e2f2dd93c0b0ed146ac01dc0292afb04e60e
kaueludovico <kaueludovico@outlook.com>
2023-01-04 16:06:46 -0300

    Initial Commit

---
+++ HouseKeeper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HouseKeeper
+//
+//  Created by Kaue Ludovico on 04/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
